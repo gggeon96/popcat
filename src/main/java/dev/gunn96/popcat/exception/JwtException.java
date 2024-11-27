4284f63788eb8a237569901e26494eda18481e1f
gggeon96 <gggeon96@github.com>
2024-11-27 16:27:14 +0900

    feat: JWT provider
    - add TokenClaims domain
    - add JwtProvider that validate&generate token
    - add Exception while processing jwt token

---
+++ src/main/java/dev/gunn96/popcat/exception/JwtException.java
@@ -0,0 +1,27 @@
+package dev.gunn96.popcat.exception;
+
+public class JwtException extends RuntimeException {
+    public JwtException(String message) {
+        super(message);
+    }
+
+    public JwtException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public static class EmptyTokenException extends JwtException {
+        public EmptyTokenException() {
+            super("Token is empty");
+        }
+    }
+
+    public static class InvalidTokenException extends JwtException {
+        public InvalidTokenException(String message) {
+            super(message);
+        }
+
+        public InvalidTokenException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+}
